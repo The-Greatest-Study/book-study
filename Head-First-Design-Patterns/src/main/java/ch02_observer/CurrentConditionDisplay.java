de455c345efb88a9e964d0724b0de227af2d096b
minho65 <skt_ks@naver.com>
2023-04-02 21:09:56 +0900

    add: add ch02 observer

---
+++ Head-First-Design-Patterns/src/main/java/ch02_observer/CurrentConditionDisplay.java
@@ -0,0 +1,26 @@
+package ch02_observer;
+
+public class CurrentConditionDisplay implements Observer, DisplayElement{
+    private WeatherData weatherData;
+    private float temperature;
+    private float humidity;
+
+    public CurrentConditionDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void display() {
+        final String msg = new StringBuilder("현재 상태 : 온도 ")
+                .append(temperature).append("F, 습도 ").append(humidity).append("%").toString();
+        System.out.println(msg);
+    }
+
+    @Override
+    public void update() {
+        this.temperature = weatherData.getTemperature();
+        this.humidity = weatherData.getHumidity();
+        display();
+    }
+}
