3333724354fe2703f7b3a3987321b1c7ecec3369
minnczi <kmj1995kr@gmail.com>
2023-04-04 23:49:54 +0900

    DELETE - deleted Minji's source folder

--- Head First Design/src/Minji/Chap02/DisplayElement.java
+++
@@ -1,5 +0,0 @@
-package Minji.Chap02;
-
-public interface DisplayElement {
-    public void display();
-}
--- Head First Design/src/Minji/Chap02/Observer.java
+++
@@ -1,5 +0,0 @@
-package Minji.Chap02;
-
-public interface Observer {
-    public void update(float temp, float humidity, float pressure);
-}
