b0d1eac91425178167bb5eb09d9330426dc81c66
swooku <sizn08@gmail.com>
2024-01-07 21:58:38 +0900

    ADD - chpater9 ver3

--- Head-First-Design-Pattern/Head-First-Design-Pattern/src/main/java/swooku/chapter09_collection_management/Waitress.java
+++ Head-First-Design-Pattern/Head-First-Design-Pattern/src/main/java/swooku/chapter09_collection_management/Waitress.java
@@ -5,20 +5,25 @@ import java.util.Iterator;
 public class Waitress {
     Menu pancakeHouseMenu;
     Menu dinerMenu;
+    Menu cafeMenu;
 
-    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu) {
+    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu, Menu cafeMenu) {
         this.pancakeHouseMenu = pancakeHouseMenu;
         this.dinerMenu = dinerMenu;
+        this.cafeMenu = cafeMenu;
     }
 
     public void printMenu() {
         Iterator pancakIterator = pancakeHouseMenu.createIterator();
         Iterator dinerIterator = dinerMenu.createIterator();
+        Iterator cafeIterator = cafeMenu.createIterator();
 
         System.out.println("메뉴\n----\n아침 메뉴");
         printMenu(pancakIterator);
         System.out.println("\n점심 메뉴");
         printMenu(dinerIterator);
+        System.out.println("\n저녁 메뉴");
+        printMenu(cafeIterator);
     }
 
     private void printMenu(Iterator iterator) {
