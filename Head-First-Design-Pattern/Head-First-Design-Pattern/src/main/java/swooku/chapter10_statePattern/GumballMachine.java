e7b626c5a9575c98157dedde0874bfdca834a193
swooku <sizn08@gmail.com>
2024-01-14 04:10:47 +0900

    ADD - chapter10

---
+++ Head-First-Design-Pattern/Head-First-Design-Pattern/src/main/java/swooku/chapter10_statePattern/GumballMachine.java
@@ -0,0 +1,62 @@
+package swooku.chapter10_statePattern;
+
+import lombok.Getter;
+import swooku.chapter10_statePattern.StateType.*;
+
+@Getter
+public class GumballMachine {
+    State soldOutState;
+    State noQuarterState;
+    State hasQuarterState;
+    State soldState;
+    State winnerState;
+
+    State state;
+    int count = 0;
+
+    public GumballMachine(int numberGumballs) {
+        soldOutState = new SoldOutState(this);
+        noQuarterState = new NoQuarterState(this);
+        hasQuarterState = new HasQuarterState(this);
+        soldState = new SoldState(this);
+        winnerState = new WinnerState(this);
+
+        this.count = numberGumballs;
+        if(numberGumballs > 0) {
+            state = noQuarterState;
+        } else {
+            state = soldOutState;
+        }
+    }
+
+    public void insertQuarter() {
+        state.insertQuarter();
+    }
+
+    public void ejectQuarter() {
+        state.ejectQuarter();
+    }
+
+    public void turnCrank() {
+       state.turnCrank();
+       state.dispense();
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public void releaseBall() {
+        System.out.println("알멩이를 내보내고 있습니다.");
+        if(count > 0) {
+            count = count - 1;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "\n주식회사 왕뽑기" + "\n자바로 돌아가는 최신형 뽑기 기계"
+                            + "\n남은 개수 : " + this.count + "개"
+                            + "\n동전 투입 대기중\n";
+    }
+}
---
+++ Head-First-Design-Pattern/Head-First-Design-Pattern/src/main/java/swooku/chapter10_statePattern/StateType/NoQuarterState.java
@@ -0,0 +1,34 @@
+package swooku.chapter10_statePattern.StateType;
+
+import swooku.chapter10_statePattern.GumballMachine;
+import swooku.chapter10_statePattern.State;
+
+public class NoQuarterState implements State{
+    GumballMachine gumballMachine;
+
+    public NoQuarterState(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+
+    @Override
+    public void insertQuarter() {
+        System.out.println("동전을 넣으셨습니다.");
+        gumballMachine.setState(gumballMachine.getHasQuarterState());
+    }
+
+    @Override
+    public void ejectQuarter() {
+        System.out.println("동전을 넣어주세요.");
+    }
+
+    @Override
+    public void turnCrank() {
+        System.out.println("동전을 넣어주세요.");
+    }
+
+    @Override
+    public void dispense() {
+        System.out.println("동전을 넣어주세요.");
+    }
+    
+}
