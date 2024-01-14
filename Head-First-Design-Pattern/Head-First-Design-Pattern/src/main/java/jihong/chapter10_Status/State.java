8c2d6b2dcb9c39c0a8532170dc2c200c4be6b35f
JiHongKim15 <15computer@hanmail.net>
2024-01-14 18:26:16 +0900

    feat: chpater 10 status jihong

---
+++ Head-First-Design-Pattern/Head-First-Design-Pattern/src/main/java/jihong/chapter10_Status/State.java
@@ -0,0 +1,12 @@
+package jihong.chapter10_Status;
+
+public interface State {
+    void insertQuarter();
+    void ejectQuarter();
+    void turnCrank();
+
+    void dispense();
+
+    default void refill(){};
+
+}
