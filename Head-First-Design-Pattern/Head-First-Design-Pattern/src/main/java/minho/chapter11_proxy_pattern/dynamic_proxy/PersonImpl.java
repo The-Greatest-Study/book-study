ec283244731775df8cb83e36262d3f1b61d3f0f3
minho65 <skt_ks@naver.com>
2024-01-21 23:58:50 +0900

    feat: Add dynamic proxy

---
+++ Head-First-Design-Pattern/Head-First-Design-Pattern/src/main/java/minho/chapter11_proxy_pattern/dynamic_proxy/PersonImpl.java
@@ -0,0 +1,51 @@
+package minho.chapter11_proxy_pattern.dynamic_proxy;
+
+public class PersonImpl implements Person {
+
+    String name;
+    String gender;
+    String interests;
+    int rating;
+    int ratingCount;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getGender() {
+        return gender;
+    }
+
+    @Override
+    public String getInterests() {
+        return interests;
+    }
+
+    @Override
+    public int getGeekRating() {
+        return (ratingCount > 0) ? rating/ratingCount : 0;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public void setInterests(String interests) {
+        this.interests = interests;
+    }
+
+    @Override
+    public void setGeekRating(int rating) {
+        this.rating += rating;
+        ratingCount++;
+    }
+}
